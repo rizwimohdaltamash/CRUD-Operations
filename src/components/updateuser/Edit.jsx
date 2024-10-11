096daa9667bdd5390ac97d375b621f058aac2ab1
AltamashRizwi <rizwimohdaltamash@gmail.com>
2024-10-11 20:36:24 +0530

    changes made

--- src/components/updateuser/Edit.jsx
+++ src/components/updateuser/Edit.jsx
@@ -23,7 +23,7 @@ const Edit = () => {
   }
 
   useEffect(()=>{
-    axios.get(`http://localhost:8000/api/getone/${id}`)
+    axios.get(`https://crud-backend-nu.vercel.app/api/getone/${id}`)
     .then((response)=>{
      setUser(response.data);
     })
@@ -34,7 +34,7 @@ const Edit = () => {
 
   const submitForm = async(e)=>{
     e.preventDefault();
-    await axios.put(`http://localhost:8000/api/update/${id}`,user)
+    await axios.put(`https://crud-backend-nu.vercel.app/api/update/${id}`,user)
     .then((response)=>{
     toast.success(response.data.msg,{position:"top-right"});
     navigate("/");
